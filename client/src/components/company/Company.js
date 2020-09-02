d4ca73940b58652d0bec925f5c387b10853bdaff
SorinTanasie <tanasie.sorin1@gmail.com>
2020-09-02 19:49:28 +0200

    design

--- client/src/components/company/Company.js
+++ client/src/components/company/Company.js
@@ -9,6 +9,8 @@ import UpdateDepartamentModal from './UpdateDepartamentModal';
 import UpdateLabelModal from './UpdateLabelModal';
 import AddEmployeeModal from './AddEmployeeModal';
 
+import './company.scss'
+
 // Redux
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -102,75 +104,24 @@ const Company = ({ company }) => {
 				<button onClick={handleOpenModal}>create new company</button>
 			) : (
 				<div>
-					<p>company name: {company[0].companyName} </p>
-					<p>adress {company[0].adress} </p>
+					<div className="company-header">
+					<h1 className="company-name">{company[0].companyName} </h1>
+					<p className='company-adress'>adress {company[0].adress} </p>
+					</div>
+					<div className="company-container">
+					{/* <button
+						onClick={() => {
+							setCompanyId(company[0]._id);
+							handleOpenUpdate();
+						}}
+					>
+						edit company
+					</button> */}
+					<div className="department">
+					<div className="department-header">
 					<h3>Departaments </h3>
 
-					{company[0].departaments == undefined ? (
-						<p>no departament</p>
-					) : (
-						company[0].departaments.map((departament) => (
-							<div key={departament._id}>
-								<p>departament name: {departament.departamentName}</p>
-								<p>departament manager: {departament.departamentManager}</p>
-								{
-									departament.departamentEmployees == null || 
-									departament.departamentEmployees == undefined || 
-									departament.departamentEmployees.length == 0  ? '' : (
-										departament.departamentEmployees.map(employee => (
-											<p>Employee Email: {employee.email} </p>
-										))
-									)
-								}
-
-                <button
-                  onClick={() => {
-                    handleOpenUpdateDepartament();
-                    setDepartamentId(departament._id);
-                    setCompanyId(company[0]._id);
-                  }}
-                >edit</button>
-                <button
-                  onClick={e => store.dispatch(handleDeleteDepartament(company[0]._id, departament._id))}
-                >delete</button>
-
-								<button
-									onClick={() => {
-										setCompanyId(company[0]._id);
-										setDepartamentId(departament._id);
-										handleOpenAddEE();
-									}}
-								>
-									add employee
-								</button>
-							</div>
-						))
-					)}
-
-					<h3>Labels:</h3>
-					{company[0].labelStatus == undefined ? (
-						''
-					) : (
-						company[0].labelStatus.map((label) => (
-							<div key={label._id}>
-								<p>labelPriority: {label.labelPriority}</p>
-								<p>statusName: {label.statusName}</p>
-								<p>color: {label.color}</p>
-                <button
-                  onClick={() => {
-                    handleOpenUpdateLabel();
-                    setLabelId(label._id);
-                    setCompanyId(company[0]._id);
-                  }}
-                >edit</button>
-                <button
-                  onClick={e => store.dispatch(handleDeleteLabel(company[0]._id, label._id))}
-                >delete</button>
-							</div>
-						))
-					)}
-
-					<button
+					<button className="button"
 						onClick={() => {
 							setCompanyId(company[0]._id);
 							handleOpenDepartament();
@@ -179,8 +130,61 @@ const Company = ({ company }) => {
 						{' '}
 						add departament
 					</button>
+					</div>
+						<div className="departments">
+					{company[0].departaments == undefined ? (
+						<p>no departament</p>
+					) : (
+						company[0].departaments.map((departament) => (
+							<div className="individual-department" key={departament._id}>
+								<div className="department-details">
+								<p>departament name: {departament.departamentName}</p>
+								<p>departament manager: {departament.departamentManager}</p>
+								{
+									departament.departamentEmployees == null || 
+									departament.departamentEmployees == undefined || 
+									departament.departamentEmployees.length == 0  ? '' : (
+										departament.departamentEmployees.map(employee => (
+											employee==null?"":
+											<p>Employee Email: {employee.email} </p>
+										))
+									)
+								}
+								</div>
+				<div className="buttons">
+                <button className="button"
+                  onClick={() => {
+                    handleOpenUpdateDepartament();
+                    setDepartamentId(departament._id);
+                    setCompanyId(company[0]._id);
+                  }}
+                >edit</button>
+                <button className="button"
+                  onClick={e => store.dispatch(handleDeleteDepartament(company[0]._id, departament._id))}
+                >delete</button>
 
-          <button
+								<button
+								className="button"
+									onClick={() => {
+										setCompanyId(company[0]._id);
+										setDepartamentId(departament._id);
+										handleOpenAddEE();
+									}}
+								>
+									add employee
+								</button>
+								
+							</div>
+							</div>
+						
+						))
+					)}
+					</div>
+					</div>
+					<div className="labels">
+						<div className="label-header">
+					<h3 className="company-name">Labels:</h3>
+					<button className="button"
 						onClick={() => {
 							setCompanyId(company[0]._id);
 							handleOpenLabel();
@@ -189,18 +193,46 @@ const Company = ({ company }) => {
 						{' '}
 						add label
 					</button>
+					</div>
+					<div className="labels-container">
+					{company[0].labelStatus == undefined ? (
+						''
+					) : (
+						company[0].labelStatus.map((label) => (
+							<div className="individual-department" key={label._id}>
+								<div className="label-details">
+								<p>labelPriority: {label.labelPriority}</p>
+								<p>statusName: {label.statusName}</p>
+								<p>color: {label.color}</p>
+								</div>
+								<div >
+                <button
+				className="button"
+                  onClick={() => {
+                    handleOpenUpdateLabel();
+                    setLabelId(label._id);
+                    setCompanyId(company[0]._id);
+                  }}
+                >edit</button>
+                <button
+				className="button"
+                  onClick={e => store.dispatch(handleDeleteLabel(company[0]._id, label._id))}
+                >delete</button>
+							</div>
+							</div>
+						))
+					)}
 
-					<button onClick={() => store.dispatch(handleDeleteCompany(company[0]._id))}>delete company</button>
-					<button
-						onClick={() => {
-							setCompanyId(company[0]._id);
-							handleOpenUpdate();
-						}}
-					>
-						edit company
-					</button>
+					
+
+          
+					</div>
+					{/* <button onClick={() => store.dispatch(handleDeleteCompany(company[0]._id))}>delete company</button> */}
+					</div>
+				</div>
 				</div>
 			)}
+			
 		</div>
 	);
 };
